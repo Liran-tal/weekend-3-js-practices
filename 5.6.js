e484fe1446155ae75496bc459546f36dd9384b1a
Liran-tal <taliran44@gmail.com>
2021-11-13 09:54:26 +0200

    exercise 5.6 - working(?)

--- 5.6.js
+++ 5.6.js
@@ -1,11 +1,12 @@
 function maskify(str) {
 	let masked = '';
 	if (str.length > 4){
-		"#".repeat(str.length - 4);
+		str = str.split('');
+		masked = "#".repeat(str.length - 4);
+		str.splice(0, (str.length - 4), masked);
+		str = str.join('');
 	}
-	str = str.split('');
-	str.splice(0, (str.length - 4), masked);
-	return str.join('');
+	return str;
 }
 
 console.log(maskify("4556364607935616"));
